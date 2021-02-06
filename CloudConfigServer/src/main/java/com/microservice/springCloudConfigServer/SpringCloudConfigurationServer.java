46eb8677b711fb9f90b5ee353420bcf4a7dfcfa0
HELLSCREWER <abhinaykr93@gmail.com>
2021-02-06 10:55:35 +0530

    limit service communication with config server fixed.

--- CloudConfigServer/src/main/java/com/microservice/springCloudConfigServer/SpringCloudConfigurationServer.java
+++ CloudConfigServer/src/main/java/com/microservice/springCloudConfigServer/SpringCloudConfigurationServer.java
@@ -1,5 +1,6 @@
 package com.microservice.springCloudConfigServer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -8,8 +9,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class SpringCloudConfigurationServer {
 
+	@Value("${limits-service.maximum}")
+	static String cloudVar;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigurationServer.class, args);
+		System.out.println("*****************************************************"+cloudVar);
 	}
-
 }
