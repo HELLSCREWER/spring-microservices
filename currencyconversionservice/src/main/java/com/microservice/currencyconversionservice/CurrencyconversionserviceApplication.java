bb55b2f950507ffb7dbf742de39f22a9759f84ec
HELLSCREWER <abhinaykr93@gmail.com>
2021-03-07 12:44:38 +0530

    currency-conversion-service added

---
+++ currencyconversionservice/src/main/java/com/microservice/currencyconversionservice/CurrencyconversionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyconversionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyconversionserviceApplication.class, args);
+	}
+
+}
